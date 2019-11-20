8250df1eb1516f08134537e311d000a725182645
ine <enchanted13th@gmail.com>
2019-11-20 20:53:40 +0900

    [Chapter 01] ine

---
+++ Chapter01/Overload.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Overload {
+	public static void main(String[] args) {
+		int language = 3;
+		System.out.format("Language %d: I am Java! Scarier than C!\n", language);
+	}
+}
