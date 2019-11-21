c1930e3d02fa16d5cd59824ed7cde1cd6009f9ea
ine <enchanted13th@gmail.com>
2019-11-21 15:19:04 +0900

    [Chapter 02] ine

---
+++ Chapter02/pyTest01.py
@@ -0,0 +1,7 @@
+seconds_per_minute = 60;
+seconds_per_hour = seconds_per_minute * 60;
+seconds_per_day = seconds_per_hour * 24;
+print(seconds_per_hour)
+print(seconds_per_day)
+print(seconds_per_day/seconds_per_hour)
+print(seconds_per_day//seconds_per_hour)
