90d318e44f0d780ab94f12560adab8007b4df749
ine <enchanted13th@gmail.com>
2019-12-04 03:48:01 +0900

    [Chapter 03]

--- Chapter03/listTest11.py
+++ Chapter03/listTest11.py
@@ -1,12 +1,4 @@
 marxes = ['Groucho', 'Chico', 'Harpo', 'Gummo', 'Zeppo', 'Karl']
+marxes.remove('Gummo')
 
 print(marxes)
-
-del marxes[-1]
-
-print(marxes)
-print(marxes[2])
-
-del marxes[2]
-
-print(marxes[2])
