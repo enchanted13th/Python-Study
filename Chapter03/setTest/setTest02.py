44b9887e07a691ba4998745711af9d86c716f056
ine <enchanted13th@gmail.com>
2019-12-08 16:25:07 +0900

    [Chapter 03] setTest

---
+++ Chapter03/setTest/setTest02.py
@@ -0,0 +1,4 @@
+print(set('letters'))
+print(set(['Dasher', 'Dancer', 'Prancer', 'Mason-Dixon']))
+print(set(('Ummagumma', 'Echoes', 'Atom Heart Motther')))
+print(set({'apple' : 'red', 'orange' : 'orange', 'cherry' : 'red'}))
---
+++ Chapter03/setTest/setTest04.py
@@ -0,0 +1,8 @@
+drinks = {
+    'martini' : { 'vodka', 'vermouth' },
+    'black russian' : { 'vodka', 'kahlua' },
+    'white russian' : { 'cream', 'kahlua', 'vodka' },
+    'manhatten' : { 'rye', 'vermouth', 'bitters'},
+    'screwdriver' : { 'orange juice', 'vodka'},
+}
+
