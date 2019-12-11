0b7371d6feb28d7580fd52e870f37d88d6e773cc
ine <enchanted13th@gmail.com>
2019-12-11 10:53:02 +0900

    [Chapter 03] practice

--- Chapter03/practice02.py
+++ Chapter03/practice02.py
@@ -4,7 +4,26 @@ e2f = { 'dog' : 'chien',
       }
 
 print(e2f)
-print(e2f.get('walrus'))
+print(e2f['walrus'])
 
+e2f_list = list(e2f.items())
+f2e = {}
 
+for eng, france in e2f_list :
+    f2e[france] = eng
 
+print(f2e['chien'])
+
+print(e2f.keys())
+
+life = { 'animals' : { 'cats' : 'Henri',
+                       'octopi' : 'Grumpy',
+                       'emus' : 'Lucy',
+                     },
+         'plants' : {},
+         'other' : {},
+       }
+
+print(life.keys())
+print(life.get('animals').keys())
+print(life['animals']['cats'])
